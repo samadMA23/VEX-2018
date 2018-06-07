5299998dae9928cba35cbf57bcd75467450aecfa
schineaj23 <schineaj23@pwcs-edu.org>
2018-06-07 15:55:36 -0400

    New competition template for code readability sake.

---
+++ tests/skimmed_template.c
@@ -0,0 +1,30 @@
+//////////////////////////////////////
+//	File: 			skimmed_template.c	//
+//	Author: 		Andrew Schineller		//
+//	Date:				6/7/2018	3:54PM		//
+//////////////////////////////////////
+
+
+#pragma region setup
+#pragma platform(VEX2)
+#pragma competitionControl(Competition)
+#include "Vex_Competition_Includes.c"
+#pragma endregion
+
+void pre_auton()
+{
+	bDisplayCompetitonStatusOnLcd = false;
+}
+
+task autonomous()
+{
+
+}
+
+task usercontrol()
+{
+	while(true)
+	{
+
+	}
+}
