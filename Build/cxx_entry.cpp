540d23e96b573a5baa642d5f126596552b8e3bbe
schineaj23 <schineaj23@pwcs-edu.org>
2018-10-09 16:06:07 -0400

    Added Travis-CI integration, hope this works.

---
+++ Build/cxx_entry.cpp
@@ -0,0 +1,18 @@
+//C++ entry point
+#undef __INT32_TYPE__
+#undef __UINT32_TYPE__
+#define __INT32_TYPE__ long
+#define __UINT32_TYPE__ unsigned long
+
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "vexv5/v5.h"
+#include "vexv5/v5_vcs.h"
+//#include "v5_vcs_options.h"
+
+//user C++ code
+#include "../Competition/Drew/Cpp/main.cpp"
+
+
